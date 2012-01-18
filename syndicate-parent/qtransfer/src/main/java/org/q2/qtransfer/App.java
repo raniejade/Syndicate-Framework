015a3af4303ac9466e3b6e599f7a699645d7603e
Jade <raniejaderamiso@gmail.com>
2012-01-18 21:39:03 +0800

    edited some pom files

--- syndicate-parent/qtransfer/src/main/java/org/q2/qtransfer/App.java
+++ syndicate-parent/qtransfer/src/main/java/org/q2/qtransfer/App.java
@@ -20,7 +20,8 @@ public class App {
 	}
 
 	final PopupMenu popup = new PopupMenu();
-	final TrayIcon trayIcon = new TrayIcon(createImage("icon.png", "icon"));
+	final TrayIcon trayIcon = new TrayIcon(createImage("/icon.png", "icon"));
+	trayIcon.setImageAutoSize(true);
 	final SystemTray sysTray = SystemTray.getSystemTray();
 
 	MenuItem aboutMenuItem = new MenuItem("About");
@@ -32,6 +33,24 @@ public class App {
 	popup.add(exitMenuItem);
 
 	trayIcon.setPopupMenu(popup);
+
+	/*trayIcon.addMouseListener(new MouseListener() {
+		public void mouseClicked(MouseEvent e) {
+		    popup.show((Component)popup.getParent(), ((Component)popup.getParent()).getWidth(),((Component) popup.getParent()).getHeight());
+		}
+		
+		public void mouseEntered(MouseEvent e) {
+		}
+
+		public void mouseExited(MouseEvent e) {
+		}
+		
+		public void mousePressed(MouseEvent e) {
+		}
+
+		public void mouseReleased(MouseEvent e) {
+		}
+	});*/
 
 	try {
 	    sysTray.add(trayIcon);
